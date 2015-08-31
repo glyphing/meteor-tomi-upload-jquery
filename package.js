9bdde1565f1cea38252718bf627a04930547aff0
glyphing <d0001@glyphing.com>
2015-08-31 07:18:58 +0200

    dependency fix

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
-  api.use(['tap:i18n'], 'client');
+  api.use(['tap:i18n']);
   api.use(['reactive-var', 'templating'], 'client');
 
   api.addFiles('package-tap.i18n');
